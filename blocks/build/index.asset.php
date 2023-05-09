3749f3bc971ab168b7dccc3e603f27afbb6d5be9
Pea <pea@misfist.com>
2023-05-09 18:10:31 -0400

    Update classes

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-primitives'), 'version' => 'fa1674763144035e67dadb80ad84102b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-primitives'), 'version' => '35b1a7c6dd4dd14afd2b1e9379507087');

c7330ba4ce832790d822ff1681396a09a88890ff
Pea <pea@misfist.com>
2023-05-17 14:49:18 -0400

    Fix download icon size in preview

--- blocks/src/social-card/icons.js
+++ blocks/src/social-card/icons.js
@@ -20,7 +20,7 @@ const icons = {
 		</SVG>
 	),
 	download: (
-		<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+		<SVG width="24" height="24" viewBox="0 0 24 24" version="1.1">
 			<Path d="M18 11.3l-1-1.1-4 4V3h-1.5v11.3L7 10.2l-1 1.1 6.2 5.8 5.8-5.8zm.5 3.7v3.5h-13V15H4v5h16v-5h-1.5z" />
 		</SVG>
 	),
